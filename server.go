f4f1d05c998eb210b7328ac315c5f9a16b4e870c
Sébastien Boisard <sebastien.boisard@gmail.com>
2017-01-05 23:32:09 +0100

    Manage several clients by storing websocket connections

--- server.go
+++ server.go
@@ -9,10 +9,11 @@ import (
 	"math/rand"
 	"net/http"
 	"strconv"
-	"time"
+	"bufio"
+	"os"
+	"fmt"
 
 	"github.com/gorilla/websocket"
-	"fmt"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
@@ -21,11 +22,16 @@ var addr = flag.String("addr", "localhost:8080", "http service address")
 // Without parameters, default options will be applied (ReadBufferSize WriteBufferSize are set to 4096
 var upgrader = websocket.Upgrader{} // use default options
 
+type WebSocketStore struct {
+	token  string
+	socket *websocket.Conn
+}
+
+var clients []WebSocketStore
+
 func handleActionFunc(w http.ResponseWriter, r *http.Request) {
 
-	token := r.URL.Query().Get("token")
-
-	fmt.Println("token=", token)
+	t := r.URL.Query().Get("token")
 
 	// Upgrade upgrades the HTTP server connection to the WebSocket protocol.
 	// c (i.e. Conn type) represents a WebSocket connection.
@@ -35,69 +41,96 @@ func handleActionFunc(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	defer c.Close()
+	clients = append(clients, WebSocketStore{token: t, socket: c})
 
-	for {
+	// TODO: find when to close the WebSocket
+	//defer c.Close()
+}
 
-		id := strconv.Itoa(rand.Intn(4))
+func sendAction(c *websocket.Conn) {
 
-		var action Action
+	id := strconv.Itoa(rand.Intn(4))
 
-		switch id {
-		case "1":
-			action = Action{
-				Name:       "action" + id,
-				Parameters: []ActionParameter{{Name: "param", Value: int(1)}},
-			}
+	var action Action
 
-		case "2":
-			action = Action{
-				Name:       "action" + id,
-				Parameters: []ActionParameter{{Name: "param", Value: "2"}},
-			}
-
-		case "3":
-			action = Action{
-				Name:       "action" + id,
-				Parameters: []ActionParameter{{Name: "param1", Value: 3.3}, {Name: "param2", Value: true}},
-			}
-
-		default:
-			action = Action{
-				Name: "action" + id,
-			}
+	switch id {
+	case "1":
+		action = Action{
+			Name:       "action" + id,
+			Parameters: []ActionParameter{{Name: "param", Value: int(1)}},
 		}
 
-		jsonAction, err := json.Marshal(action)
-		if err != nil {
-			log.Println("marshal:", err)
-			return
+	case "2":
+		action = Action{
+			Name:       "action" + id,
+			Parameters: []ActionParameter{{Name: "param", Value: "2"}},
 		}
 
-		err = c.WriteMessage(websocket.TextMessage, []byte(jsonAction))
-		if err != nil {
-			log.Println("write:", err)
-			break
+	case "3":
+		action = Action{
+			Name:       "action" + id,
+			Parameters: []ActionParameter{{Name: "param1", Value: 3.3}, {Name: "param2", Value: true}},
 		}
 
-		log.Printf("sent: %s", jsonAction)
-
-		// messageType int, message []byte, err error
-		messageType, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			break
+	default:
+		action = Action{
+			Name: "action" + id,
 		}
-
-		log.Printf("recv: %s (type=%d)", message, messageType)
-
-		time.Sleep(time.Second * 2)
 	}
+
+	jsonAction, err := json.Marshal(action)
+	if err != nil {
+		log.Println("marshal:", err)
+		return
+	}
+
+	err = c.WriteMessage(websocket.TextMessage, []byte(jsonAction))
+	if err != nil {
+		log.Println("write:", err)
+		return
+	}
+
+	log.Printf("sent: %s", jsonAction)
+
+	// messageType int, message []byte, err error
+	messageType, message, err := c.ReadMessage()
+	if err != nil {
+		log.Println("read:", err)
+		return
+	}
+
+	log.Printf("recv: %s (type=%d)", message, messageType)
 }
 
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
+
+	clients = make([]WebSocketStore, 0)
+	go func() {
+		reader := bufio.NewReader(os.Stdin)
+
+		for {
+
+			for {
+				if len(clients) > 0 {
+					break
+				}
+				fmt.Println("No client for now. Press [enter] to refresh")
+				reader.ReadString('\n')
+			}
+
+			fmt.Println("List of the clients:")
+			for i, v := range clients {
+				fmt.Printf("%d- %s\n", i, v.token)
+			}
+			fmt.Print("Choose a client: ")
+			var choice int
+			fmt.Scanf("%d", &choice)
+
+			sendAction(clients[choice].socket)
+		}
+	}()
 	http.HandleFunc("/action", handleActionFunc)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
